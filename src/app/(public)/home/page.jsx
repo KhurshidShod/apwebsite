ceef52adc9e619e4ab2bc92299492032c263937f
assavlatbek <assavlatbek@gmail.com>
2023-12-26 17:11:55 +0500

    savlatbek's code

--- src/app/(public)/home/page.jsx
+++ src/app/(public)/home/page.jsx
@@ -1,5 +1,19 @@
 export default function Homee() {
   return (
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odit nemo aspernatur excepturi repellendus, doloremque, aliquam reiciendis similique atque nam alias laudantium quidem hic doloribus distinctio optio reprehenderit. Beatae ex deleniti tempore voluptatum cupiditate error quos corporis quidem, impedit quis obcaecati, iure porro dolorem amet. Porro officiis mollitia deserunt cum.</p>
-  )
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Savlatbek`s code</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odit
+        nemo aspernatur excepturi repellendus, doloremque, aliquam reiciendis
+        similique atque nam alias laudantium quidem hic doloribus distinctio
+        optio reprehenderit. Beatae ex deleniti tempore voluptatum cupiditate
+        error quos corporis quidem, impedit quis obcaecati, iure porro dolorem
+        amet. Porro officiis mollitia deserunt cum.
+      </p>
+    </>
+  );
 }
