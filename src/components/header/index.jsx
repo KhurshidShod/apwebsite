f314f6aa6d6d3eb8e50d5fb293f592518667f679
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-12-29 11:30:14 +0500

    asd

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 import { GrLanguage } from "react-icons/gr";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import Image from "next/image";
+import Magnetic from "@/commons/Magnetic/index";
+
 
 import styles from "./style.module.scss";
 
@@ -25,38 +27,54 @@ const Header = () => {
     <header className={styles.header}>
       <div className="container">
         <nav className={styles.nav__wrapper}>
+        <Magnetic>
+            <div>
             <HiOutlineMenuAlt2 size={35} />
+            </div>
+            </Magnetic>
           <div ref={sliderRef} className={styles.indicator}></div>
           <ul>
+            <Magnetic>
             <li
               onMouseEnter={(e) => sliderMouseOver(e)}
               onMouseLeave={(e) => sliderMouseLeave(e)}
             >
               Asosiy
             </li>
+            </Magnetic>
+            <Magnetic>
             <li
               onMouseEnter={(e) => sliderMouseOver(e)}
               onMouseLeave={(e) => sliderMouseLeave(e)}
             >
               Haqimda
             </li>
+            </Magnetic>
           </ul>
           <Image className={styles.logo} src="/images/ap.png" width={75} height={50} alt="" />
           <ul>
+          <Magnetic>
             <li
               onMouseEnter={(e) => sliderMouseOver(e)}
               onMouseLeave={(e) => sliderMouseLeave(e)}
             >
               Portfolio
             </li>
+            </Magnetic>
+            <Magnetic>
             <li
               onMouseEnter={(e) => sliderMouseOver(e)}
               onMouseLeave={(e) => sliderMouseLeave(e)}
             >
               Boglanish
             </li>
+            </Magnetic>
           </ul>
+          <Magnetic>
+            <div>
           <GrLanguage onClick={() => setLangsOpen(!langsOpen)} size={27} />
+            </div>
+            </Magnetic>
           <div className={`${styles.langs} ${langsOpen ? styles.visible : ''}`}>
             <button>UZ</button>
             <button>RU</button>
